12f13cf912aff3abf9115a07c57bd1d83d8dbeb6
amar5445 <amar5445@gmail.com>
2016-02-11 16:33:14 -0500

    modified files

---
+++ AngularJSClass/js/select_border.js
@@ -0,0 +1,34 @@
+function chooseBorder ()
+{
+var borderSize = window.prompt('Please select size for table border\n 3( 3 pixel border )\n 5( 5 pixel border )\n 7( 7 pixel border )\n 9( 9 pixel border )\n');
+borderSize = parseInt(borderSize); 
+switch (borderSize) {
+case 3:
+createTable(3);
+break;
+case 5:
+createTable(5);
+break;
+case 7:
+createTable(7);
+break;
+case 9:
+createTable(9);
+break;
+default:
+createTable(1);
+break;
+}
+}
+function createTable(size)
+{ 
+var result='<h4>Northern New Jersey Profit</h4>';
+	result+='<table border ='+size+'> <tr> <td>	</td> <th>April</th> <th>May</th> <th>June</th>  </tr>'; 
+							result+=' <tr> <th>Montclair</th> <td>100</td> <td>80</td> <td>90</td> </tr>';
+							result+=' <tr> <th>Clifton</th> <td>79</td> <td>80</td> <td>100</td> </tr>';
+							result+=' <tr> <th>Newark</th> <td>100</td> <td>95</td> <td>91</td> </tr>';
+    result+='</table>'; 
+	result+='<h1>Press reload or refresh</h1>';
+	document.getElementById( "demo").innerHTML=result;
+
+}
